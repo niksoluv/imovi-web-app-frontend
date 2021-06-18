d310f29ee9dcc56b9b29031697578be5f6bb8b2c
Glory-HunteR <85020235+Glory-HunteR@users.noreply.github.com>
2021-06-18 22:05:32 +0300

    Added footer text

--- src/App.js
+++ src/App.js
@@ -49,7 +49,10 @@ class App extends React.Component {
 						
 					</Container>
 					<footer className="footer">
-						Footer
+						<h5><strong>Imovi</strong></h5>
+						<h7>Movie catalog web application based on React JS, Redux and .NET Web API</h7><br/>
+						<h7>Developed by students of group PZ-32</h7><br/>
+						<h6><strong>Vitaliy Hladkyi and Yaroslav Hura</strong></h6>
 					</footer>
 				</div>
 			</BrowserRouter>
